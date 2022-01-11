792390c2984d8544816ca69d1e2ceb29dba028b7
fjebaker <fergusbkr@gmail.com>
2022-01-11 10:11:18 +0100

    specialisations and inner_radius

--- src/metric-params.jl
+++ src/metric-params.jl
@@ -7,21 +7,21 @@ abstract type AbstractMetricParams{T} end
 
 
 """
-    geodesic_eq(m::AbstractMetricParams{T})
+    geodesic_eq(m::AbstractMetricParams{T}, u, v)
+    geodesic_eq!(m::AbstractMetricParams{T}, u, v)
 
-Return components of the geodesic equation for the metric described by `m`.
+Calculate the acceleration components of the geodesic equation given a position `u`, a velocity `v`, and a metric `m`.
 """
-geodesic_eq(m::AbstractMetricParams{T}) where {T} = error("Not implemented for metric parameters $(typeof(m))")
-
+geodesic_eq(m::AbstractMetricParams{T}, u, v) where {T} = error("Not implemented for metric parameters $(typeof(m))")
+geodesic_eq!(m::AbstractMetricParams{T}, u, v) where {T} = error("Not implemented for metric parameters $(typeof(m))")
 
 """
-    constrain(m::AbstractMetricParams{T}, u, v; μ=0.0)
+    constrain(m::AbstractMetricParams{T}, u, v; μ::T=0.0f0)
 
 Give time component which would constrain a velocity vector `v` at position `x` to be a
 geodesic with mass `μ`.
 """
-constrain(m::AbstractMetricParams{T}, u, v; μ=0.0) where {T} = error("Not implemented for metric parameters $(typeof(m))")
-
+constrain(m::AbstractMetricParams{T}, u, v; μ::T=0.0) where {T} = error("Not implemented for metric parameters $(typeof(m))")
 
 """
     on_chart(m::AbstractMetricParams{T}, u::AbstractVector)
@@ -32,6 +32,16 @@ Returns false is `u` is a singularity.
 """
 on_chart(m::AbstractMetricParams{T}, u) where {T} = error("Not implemented for metric parameters $(typeof(m))")
 
+
+"""
+    inner_radius(m::AbstractMetricParams{T})::T
+
+Return the innermost valid coordinate relative to the origin, for use in geodesic tracing.
+
+This usually represents some property of the metric, e.g. event horizon radius in Kerr/Schwarzschild metrics, or 
+throat diameter in worm hole metrics.
+"""
+inner_radius(m::AbstractMetricParams{T})::T where {T} = 0.0
 
 """
     metric_type(m::AbstractMetricParams{T})
