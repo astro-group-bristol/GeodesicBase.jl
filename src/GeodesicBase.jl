e5858774ca314ff20f01fec5f56e45465bfd206f
fjebaker <fergusbkr@gmail.com>
2022-02-23 09:46:45 +0000

    removed geodesic params

--- src/GeodesicBase.jl
+++ src/GeodesicBase.jl
@@ -5,11 +5,10 @@ include("metric-params.jl")
 # contains the full metric components (this type needed for DiffGeoSymbolics)
 abstract type AbstractMetric{T} <: AbstractMatrix{T} end
 
-abstract type AbstractGeodesicParams{M,T} end 
 
 metric_params(m::AbstractMetric{T}) where {T} = error("Not implemented for metric $(typeof(m))")
 
-export AbstractMetricParams, AbstractGeodesicParams, metric_params
+export AbstractMetricParams, metric_params
 
 
 end # module
