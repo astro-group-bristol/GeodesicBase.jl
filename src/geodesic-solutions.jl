0931d5c107ffbdaa59607bf5861cad0ba1d7b1c4
fjebaker <fergusbkr@gmail.com>
2022-03-16 15:21:33 +0000

    updated geodesic point struct and type

--- src/geodesic-solutions.jl
+++ src/geodesic-solutions.jl
@@ -1,10 +1,18 @@
-abstract type AbstractGeodesic{T} end
+abstract type AbstractGeodesicPoint{T} end
 
-@with_kw struct GeodesicPoint{T,P} <: AbstractGeodesic{T}
+@with_kw struct GeodesicPoint{T} <: AbstractGeodesicPoint{T}
+    retcode::Symbol
+    t::T
     u::AbstractVector{T}
     v::AbstractVector{T}
-    t::T
-    p::P
+    # we don't store the problem parameters
+    # and can create a specialistion for the carter method
+    # then provide dispatched accessor methods
+    # p::P
+end
+
+function geodesic_point_type(m::AbstractMetricParams{T}) where {T}
+    GeodesicPoint{T}
 end
 
 # TODO: GeodesicPath structure for the full geodesic path
@@ -21,15 +29,21 @@ function unpack_solution(simsol::SciMLBase.AbstractEnsembleSolution{T,N,V}) wher
     map(unpack_solution, simsol)
 end
 
-function get_endpoint(m::AbstractMetricParams{T}, sol::SciMLBase.AbstractODESolution{T,N,S}) where {T,N,S}
-    us, ts, p = unpack_solution(sol)
+function get_endpoint(
+    m::AbstractMetricParams{T},
+    sol::SciMLBase.AbstractODESolution{T,N,S},
+) where {T,N,S}
+    us, ts, _ = unpack_solution(sol)
     u = us[end].x[2]
     v = us[end].x[1]
     t = ts[end]
-    GeodesicPoint(u, v, t, p)
+    GeodesicPoint(sol.retcode, t, u, v)
 end
 
-function get_endpoint(m::AbstractMetricParams{T}, simsol::SciMLBase.AbstractEnsembleSolution{T,N,S}) where {T,N,S}
+function get_endpoint(
+    m::AbstractMetricParams{T},
+    simsol::SciMLBase.AbstractEnsembleSolution{T,N,S},
+) where {T,N,S}
     map(sol -> get_endpoint(m, sol), simsol)
 end
 
