0cb60aeba81a2fe21e363824ae3fe86dbb52a15d
Fergus Baker <fergusbkr@gmail.com>
2022-04-01 19:21:50 +0100

    E & Lz Overloads (#6)
    * add E and Lz overloads
    
    * bump version
    
    * add overload docs

--- src/physical-quantities.jl
+++ src/physical-quantities.jl
@@ -6,6 +6,7 @@
 
 """
     E(m::AbstractMatrix{T}, v) 
+    E(m::AbstractMetricParams{T}, u, v)
     
 Compute the energy for a numerically evaluated metric, and some velocity four vector `v`,
 ```math
@@ -18,10 +19,12 @@ the mass ``\\mu`` needs to be known to compute ``\\mu v^\\nu = p^\\nu``.
 function E(metric::AbstractMatrix{T}, v) where {T}
     T(@inbounds -(metric[1, 1] * v[1] + metric[1, 4] * v[4]))
 end
+E(m::AbstractMetricParams{T}, u, v) where {T} = E(metric(m, u), v)
 
 
 """
     Lz(m::AbstractMatrix{T}, v)
+    Lz(m::AbstractMetricParams{T}, u, v) 
 
 Compute the angular momentum for a numerically evaluated metric, and some velocity four vector `v`.
 ```math
@@ -31,3 +34,4 @@ L_z = p_\\phi = - g_{\\phi\\nu} p^\\nu.
 function Lz(metric::AbstractMatrix{T}, v) where {T}
     T(@inbounds metric[4, 4] * v[4] + metric[1, 4] * v[1])
 end
+Lz(m::AbstractMetricParams{T}, u, v) where {T} = Lz(metric(m, u), v)
